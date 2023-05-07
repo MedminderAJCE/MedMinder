bc3d28dda121463633dd0dba0ba81e8ccb2114f0
SreelakshmiAnish <sreelakshmianish20@gmail.com>
2023-05-07 17:54:11 +0530

    alarm clock config

--- lib/screens/addmedicine.dart
+++ lib/screens/addmedicine.dart
@@ -6,7 +6,6 @@ class AddMedicine extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     return Scaffold(
       body: Center(
           child: SizedBox(
@@ -14,50 +13,71 @@ class AddMedicine extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const TextField(
+                  TextField(
                     decoration: InputDecoration(
                       hintText: "Enter Medicine Name",
+                      disabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(21),
+                          borderSide: const BorderSide(
+                            color: Color(0xFF00E5FF),
+                          )),
                       focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                        color: Color(0xFF00E5FF),
-                      )),
+                          borderRadius: BorderRadius.circular(21),
+                          borderSide: const BorderSide(
+                            color: Color(0xFF00E5FF),
+                          )),
                       enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                        color: Color(0xFF00E5FF),
-                      )),
+                          borderRadius: BorderRadius.circular(21),
+                          borderSide: const BorderSide(
+                            color: Color(0xFF00E5FF),
+                          )),
                     ),
                   ),
                   Container(
                     height: 30,
                   ),
-                  const TextField(
+                  TextField(
                     keyboardType: TextInputType.phone,
                     decoration: InputDecoration(
                       hintText: "Enter Medicine Dosage",
+                      disabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(21),
+                          borderSide: const BorderSide(
+                            color: Color(0xFF00E5FF),
+                          )),
                       focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                        color: Color(0xFF00E5FF),
-                      )),
+                          borderRadius: BorderRadius.circular(21),
+                          borderSide: const BorderSide(
+                            color: Color(0xFF00E5FF),
+                          )),
                       enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                        color: Color(0xFF00E5FF),
-                      )),
+                          borderRadius: BorderRadius.circular(21),
+                          borderSide: const BorderSide(
+                            color: Color(0xFF00E5FF),
+                          )),
                     ),
                   ),
                   Container(
                     height: 30,
                   ),
-                  const TextField(
+                  TextField(
                     decoration: InputDecoration(
                       hintText: "Enter Medicine Course",
+                      disabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(21),
+                          borderSide: const BorderSide(
+                            color: Color(0xFF00E5FF),
+                          )),
                       focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                        color: Color(0xFF00E5FF),
-                      )),
+                          borderRadius: BorderRadius.circular(21),
+                          borderSide: const BorderSide(
+                            color: Color(0xFF00E5FF),
+                          )),
                       enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                        color: Color(0xFF00E5FF),
-                      )),
+                          borderRadius: BorderRadius.circular(21),
+                          borderSide: const BorderSide(
+                            color: Color(0xFF00E5FF),
+                          )),
                     ),
                   ),
                   Container(
@@ -91,124 +111,12 @@ class AddMedicine extends StatelessWidget {
                             fontSize: 20,
                             color: Colors.white)),
                     style: ElevatedButton.styleFrom(
-                        minimumSize: const Size(320.0, 50.0),
-                        backgroundColor: const Color(0xFF00E5FF)),
+                      minimumSize: const Size(320.0, 50.0),
+                      backgroundColor: const Color(0xFF00E5FF),
+                    ),
                   ),
                 ],
               ))),
-=======
-    return   Scaffold(
-      body:Center(
-        child:SizedBox(
-      
-          width:300,
-        child:Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                hintText: "Enter Medicine Name",
-                disabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(21),
-                  borderSide: const BorderSide(
-                    color:  Color(0xFF00E5FF),
-                  )
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(21),
-                  borderSide: const BorderSide(
-                    color:  Color(0xFF00E5FF),
-                  )
-                ),
-               enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(21),
-                  borderSide: const BorderSide(
-                    color:  Color(0xFF00E5FF),
-                  )
-                ),
-              ),
-            ),
-            Container(height: 30,),
-             TextField(
-              keyboardType: TextInputType.phone,
-              decoration: InputDecoration(
-                hintText: "Enter Medicine Dosage",
-                disabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(21),
-                  borderSide: const BorderSide(
-                    color:  Color(0xFF00E5FF),
-                  )
-                ),
-                 focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(21),
-                  borderSide: const BorderSide(
-                    color:  Color(0xFF00E5FF),
-                  )
-                ),
-                enabledBorder:  OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(21),
-                  borderSide: const BorderSide(
-                    color:  Color(0xFF00E5FF),
-                  )
-                ),
-              ),
-            ),
-            Container(height: 30,),
-             TextField(
-              decoration: InputDecoration(
-                hintText: "Enter Medicine Course",
-                disabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(21),
-                  borderSide: const BorderSide(
-                    color:  Color(0xFF00E5FF),
-                  )
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(21),
-                  borderSide: const BorderSide(
-                    color:Color(0xFF00E5FF),
-                  )
-                ),
-               enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(21),
-                  borderSide: const BorderSide(
-                    color:Color(0xFF00E5FF),
-                  )
-                ),
-              ),
-            ),
-            Container(height: 30,),
-            ElevatedButton(onPressed: (){
-            }, 
-            child: const Text('Quantity Left',
-              style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize:20,
-                          color: Colors.white)
-            ),
-            style: ElevatedButton.styleFrom(
-              minimumSize: const Size(320.0,50.0),
-               backgroundColor: const Color(0xFF00E5FF),
-            ),
-            ),
-             Container(height: 10,),
-            ElevatedButton(onPressed: (){
-            }, child: const Text('Set Alarm',
-            style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize:20,
-                          color: Colors.white)
-                          ),
-                          style: ElevatedButton.styleFrom(
-              minimumSize: const Size(320.0,50.0),
-              backgroundColor: const Color(0xFF00E5FF),
-            ),
-
-                          ),
-          ],
-        ))),
->>>>>>> 94fb9e5145612afe5a088d6ecab4e7c7f25ec57d
     );
   }
 }
