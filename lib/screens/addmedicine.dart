4cda260bd3322fff7cd905046d034ea4dd6567a8
Susan Andrews <susanandrews2001@gmail.com>
2023-05-08 15:10:04 +0530

    fixed merge conflict- pull required

--- lib/screens/addmedicine.dart
+++ lib/screens/addmedicine.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:intl/intl.dart';
-=======
-import 'package:login/screens/setalarm.dart';
->>>>>>> bc3d28dda121463633dd0dba0ba81e8ccb2114f0
+// import 'package:intl/intl.dart';
+// import 'package:login/screens/setalarm.dart';
 
 class AddMedicine extends StatefulWidget {
   const AddMedicine({super.key});
@@ -17,7 +14,6 @@ class _AddMedicineState extends State<AddMedicine> {
       DateTimeRange(start: DateTime(2022, 11, 5), end: DateTime(2022, 12, 24));
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     final start = dateRange.start;
     final end = dateRange.end;
     final difference = dateRange.duration;
@@ -86,7 +82,7 @@ class _AddMedicineState extends State<AddMedicine> {
                     style: ElevatedButton.styleFrom(
                       backgroundColor: const Color(0xFF00E5FF),
                     ),
-                    child: Text(DateFormat('yyyy/MM/dd').format(start)),
+                    child:Text('${start.year}/${start.month}/${start.day}'),
                   ),
                 ),
                 const SizedBox(width: 12),
@@ -96,7 +92,7 @@ class _AddMedicineState extends State<AddMedicine> {
                     style: ElevatedButton.styleFrom(
                       backgroundColor: const Color(0xFF00E5FF),
                     ),
-                    child: Text(DateFormat('yyyy/MM/dd').format(end)),
+                    child: Text('${start.year}/${start.month}/${start.day}'),
                   ),
                 ),
               ],
@@ -153,119 +149,6 @@ class _AddMedicineState extends State<AddMedicine> {
       dateRange = newDateRange;
     });
   }
-=======
-    return Scaffold(
-      body: Center(
-          child: SizedBox(
-              width: 300,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: "Enter Medicine Name",
-                      disabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(21),
-                          borderSide: const BorderSide(
-                            color: Color(0xFF00E5FF),
-                          )),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(21),
-                          borderSide: const BorderSide(
-                            color: Color(0xFF00E5FF),
-                          )),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(21),
-                          borderSide: const BorderSide(
-                            color: Color(0xFF00E5FF),
-                          )),
-                    ),
-                  ),
-                  Container(
-                    height: 30,
-                  ),
-                  TextField(
-                    keyboardType: TextInputType.phone,
-                    decoration: InputDecoration(
-                      hintText: "Enter Medicine Dosage",
-                      disabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(21),
-                          borderSide: const BorderSide(
-                            color: Color(0xFF00E5FF),
-                          )),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(21),
-                          borderSide: const BorderSide(
-                            color: Color(0xFF00E5FF),
-                          )),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(21),
-                          borderSide: const BorderSide(
-                            color: Color(0xFF00E5FF),
-                          )),
-                    ),
-                  ),
-                  Container(
-                    height: 30,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: "Enter Medicine Course",
-                      disabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(21),
-                          borderSide: const BorderSide(
-                            color: Color(0xFF00E5FF),
-                          )),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(21),
-                          borderSide: const BorderSide(
-                            color: Color(0xFF00E5FF),
-                          )),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(21),
-                          borderSide: const BorderSide(
-                            color: Color(0xFF00E5FF),
-                          )),
-                    ),
-                  ),
-                  Container(
-                    height: 30,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Quantity Left',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.white)),
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size(320.0, 50.0),
-                      backgroundColor: const Color(0xFF00E5FF),
-                    ),
-                  ),
-                  Container(
-                    height: 10,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const setalarm()));
-                    },
-                    child: const Text('Set Alarm',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.white)),
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size(320.0, 50.0),
-                      backgroundColor: const Color(0xFF00E5FF),
-                    ),
-                  ),
-                ],
-              ))),
-    );
+    
   }
->>>>>>> bc3d28dda121463633dd0dba0ba81e8ccb2114f0
-}
+
