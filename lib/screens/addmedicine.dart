623cd199c56487d7125dcb2fbe3893efe445cae8
Susan Andrews <susanandrews2001@gmail.com>
2023-05-10 07:53:11 +0530

    wrapped with singlechildscrollview-check

--- lib/screens/addmedicine.dart
+++ lib/screens/addmedicine.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 //import 'package:login/screens/setalarm.dart';
-// import 'package:intl/intl.dart';
 //import 'package:login/screens/setalarm.dart';
 import 'package:login/screens/timepicker.dart';
 
@@ -34,236 +33,238 @@ class _AddMedicineState extends State<AddMedicine> {
       body: Center(
           child: SizedBox(
         width: 300,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                hintText: "Enter Medicine Name",
-                disabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(21),
-                    borderSide: const BorderSide(
-                      color: Color(0xFF00E5FF),
-                    )),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(21),
-                    borderSide: const BorderSide(
-                      color: Color(0xFF00E5FF),
-                    )),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(21),
-                    borderSide: const BorderSide(
-                      color: Color(0xFF00E5FF),
-                    )),
-              ),
-            ),
-            Container(
-              height: 30,
-            ),
-            TextField(
-              keyboardType: TextInputType.phone,
-              decoration: InputDecoration(
-                hintText: "Enter Medicine Dosage",
-                disabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(21),
-                    borderSide: const BorderSide(
-                      color: Color(0xFF00E5FF),
-                    )),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(21),
-                    borderSide: const BorderSide(
-                      color: Color(0xFF00E5FF),
-                    )),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(21),
-                    borderSide: const BorderSide(
-                      color: Color(0xFF00E5FF),
-                    )),
-              ),
-            ),
-            Container(
-              height: 30,
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: pickDateRange,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF00E5FF),
-                    ),
-                    child:Text('${start.year}/${start.month}/${start.day}'),
-                  ),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Enter Medicine Name",
+                  disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(21),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF00E5FF),
+                      )),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(21),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF00E5FF),
+                      )),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(21),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF00E5FF),
+                      )),
                 ),
-                const SizedBox(width: 12),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: pickDateRange,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF00E5FF),
-                    ),
-                    child: Text('${start.year}/${start.month}/${start.day}'),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-            Text(
-              'Course Duration :${difference.inDays} days',
-              style: const TextStyle(fontSize: 20),
-            ),
-            Container(height: 18),
-            // ElevatedButton(
-            //   onPressed: () {},
-            //   style: ElevatedButton.styleFrom(
-            //     minimumSize: const Size(320.0, 50.0),
-            //     backgroundColor: const Color(0xFF00E5FF),
-            //   ),
-            //   child: const Text('Quantity Left',
-            //       style: TextStyle(
-            //           fontWeight: FontWeight.bold,
-            //           fontSize: 20,
-            //           color: Colors.white)),
-            // ),
-          
-            // SingleChildScrollView(
-            //   child: Padding(
-            //     padding: const EdgeInsets.symmetric(horizontal: 20),
-            //     child: Column(children: [
-            //       const SizedBox(height: 50, ),
-            //       Container(
-            //         width:1000,
-            //         height:35,
-            //         decoration: const BoxDecoration(
-            //           color: Color(0xFF00E5FF)
-            //         ),
-            //         child: Padding(
-            //           padding: const EdgeInsets.symmetric(horizontal: 10),
-            //           child: Row(
-            //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //             children: const [
-            //             Text('selectoption',
-                        
-            //             ),
-            //             Icon(Icons.arrow_drop_down),
-            //           ],
-            //           ),
-            //         ),
-            //       ),
-            //       ListView(
-            //         primary: true,
-            //         shrinkWrap: true,
-            //         children:items.map((e) => Container(
-            //           decoration: const BoxDecoration(
-            //           color: Color(0xFF00E5FF)
-            //         ),
-            //           child: Padding(
-            //             padding: const EdgeInsets.all(8.0),
-            //             child:InkWell(onTap: () {
-            //               items=e as List<String>;
-            //               setState(() {
-            //                 selectoption=e;
-            //               });
-            //             },
-                        
-            //             child: Text(e),
-            //             ),
-            //           ),
-            //         )).toList(),
-            //       ),
-            //     ]
-            //     ),
-
-            //   ),
-            // ),
-            Container(
-              height: 10,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context)
-                   .push(MaterialPageRoute(builder: (context) =>const timepicker()));
-              },
-               style: ElevatedButton.styleFrom(
-                minimumSize: const Size(320.0, 50.0),
-                backgroundColor: const Color(0xFF00E5FF),
               ),
-              child: const Text('Set Alarm',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.white)),
-                     
-            ),
-             Container(
-              height: 20,
-            ),
-
-
-
               Container(
-                width: 300,
-              decoration: const BoxDecoration(
-              color:Color(0xFF00E5FF),
+                height: 30,
               ),
-                child: DropdownButton(
-                  underline: Container(),
-                  isExpanded: true,
-                  alignment: Alignment.centerLeft,
-                hint: const SizedBox(
-                  
-                  child: Center(child: Text("Refill Reminder",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),))),
-                dropdownColor: const Color(0xFF00E5FF),
-                icon: const Icon(Icons.arrow_drop_down),
-                iconSize: 36,
-                
-                value: valuechoose,
-                
-                onChanged: (newValue){
-                  setState(() {
-                    valuechoose=newValue as String? ;
-                  });
-                }, 
-                items:listItem.map((valueItem){
-                  return DropdownMenuItem(
-                    
-                    
-                    value:valueItem,
-                    child:Padding(
-                       padding: const EdgeInsets.symmetric(horizontal: 10),
-                   
-                      child: Row(
-                        //  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                         
-                        children: [
-                          // alignment: Alignment.centerLeft,
-                          
-                          Text(valueItem ,
-                          
-                           style: const TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.white,
-                                    
-                                    // fontStyle: FontStyle.italic,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                        ],
+              TextField(
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                  hintText: "Enter Medicine Dosage",
+                  disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(21),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF00E5FF),
+                      )),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(21),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF00E5FF),
+                      )),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(21),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF00E5FF),
+                      )),
+                ),
+              ),
+              Container(
+                height: 30,
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: pickDateRange,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF00E5FF),
                       ),
+                      child:Text('${start.year}/${start.month}/${start.day}'),
                     ),
-                      );
-                      })
-                  .toList(),
-                  //   ),
-                  //   );
-                  // }).toList(),
-                          
-              
-                          ),
+                  ),
+                  const SizedBox(width: 12),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: pickDateRange,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF00E5FF),
+                      ),
+                      child: Text('${start.year}/${start.month}/${start.day}'),
+                    ),
+                  ),
+                ],
               ),
-
-          ],
+              const SizedBox(height: 16),
+              Text(
+                'Course Duration :${difference.inDays} days',
+                style: const TextStyle(fontSize: 20),
+              ),
+              Container(height: 18),
+              // ElevatedButton(
+              //   onPressed: () {},
+              //   style: ElevatedButton.styleFrom(
+              //     minimumSize: const Size(320.0, 50.0),
+              //     backgroundColor: const Color(0xFF00E5FF),
+              //   ),
+              //   child: const Text('Quantity Left',
+              //       style: TextStyle(
+              //           fontWeight: FontWeight.bold,
+              //           fontSize: 20,
+              //           color: Colors.white)),
+              // ),
+            
+              // SingleChildScrollView(
+              //   child: Padding(
+              //     padding: const EdgeInsets.symmetric(horizontal: 20),
+              //     child: Column(children: [
+              //       const SizedBox(height: 50, ),
+              //       Container(
+              //         width:1000,
+              //         height:35,
+              //         decoration: const BoxDecoration(
+              //           color: Color(0xFF00E5FF)
+              //         ),
+              //         child: Padding(
+              //           padding: const EdgeInsets.symmetric(horizontal: 10),
+              //           child: Row(
+              //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //             children: const [
+              //             Text('selectoption',
+                          
+              //             ),
+              //             Icon(Icons.arrow_drop_down),
+              //           ],
+              //           ),
+              //         ),
+              //       ),
+              //       ListView(
+              //         primary: true,
+              //         shrinkWrap: true,
+              //         children:items.map((e) => Container(
+              //           decoration: const BoxDecoration(
+              //           color: Color(0xFF00E5FF)
+              //         ),
+              //           child: Padding(
+              //             padding: const EdgeInsets.all(8.0),
+              //             child:InkWell(onTap: () {
+              //               items=e as List<String>;
+              //               setState(() {
+              //                 selectoption=e;
+              //               });
+              //             },
+                          
+              //             child: Text(e),
+              //             ),
+              //           ),
+              //         )).toList(),
+              //       ),
+              //     ]
+              //     ),
+        
+              //   ),
+              // ),
+              Container(
+                height: 10,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context)
+                     .push(MaterialPageRoute(builder: (context) =>const timepicker()));
+                },
+                 style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(320.0, 50.0),
+                  backgroundColor: const Color(0xFF00E5FF),
+                ),
+                child: const Text('Set Alarm',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.white)),
+                       
+              ),
+               Container(
+                height: 20,
+              ),
+        
+        
+        
+                Container(
+                  width: 300,
+                decoration: const BoxDecoration(
+                color:Color(0xFF00E5FF),
+                ),
+                  child: DropdownButton(
+                    underline: Container(),
+                    isExpanded: true,
+                    alignment: Alignment.centerLeft,
+                  hint: const SizedBox(
+                    
+                    child: Center(child: Text("Refill Reminder",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),))),
+                  dropdownColor: const Color(0xFF00E5FF),
+                  icon: const Icon(Icons.arrow_drop_down),
+                  iconSize: 36,
+                  
+                  value: valuechoose,
+                  
+                  onChanged: (newValue){
+                    setState(() {
+                      valuechoose=newValue as String? ;
+                    });
+                  }, 
+                  items:listItem.map((valueItem){
+                    return DropdownMenuItem(
+                      
+                      
+                      value:valueItem,
+                      child:Padding(
+                         padding: const EdgeInsets.symmetric(horizontal: 10),
+                     
+                        child: Row(
+                          //  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                           
+                          children: [
+                            // alignment: Alignment.centerLeft,
+                            
+                            Text(valueItem ,
+                            
+                             style: const TextStyle(
+                                      fontSize: 18,
+                                      color: Colors.white,
+                                      
+                                      // fontStyle: FontStyle.italic,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                          ],
+                        ),
+                      ),
+                        );
+                        })
+                    .toList(),
+                    //   ),
+                    //   );
+                    // }).toList(),
+                            
+                
+                            ),
+                ),
+        
+            ],
+          ),
         ),
       )),
     );
