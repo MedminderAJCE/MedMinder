9b3ff9e9d89f6b068ef39d04db2730bb09dff3c7
sumayya16 <sumayyamaheen2024@cs.ajce.in>
2023-05-13 13:38:44 +0530

    Caretaker Page

--- lib/screens/firstpage.dart
+++ lib/screens/firstpage.dart
@@ -1,14 +1,72 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-class Caretaker extends StatelessWidget {
-  const Caretaker({super.key});
+class Caretaker extends StatefulWidget {
+  const Caretaker({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("layer to include the caretaker info"),
-      ),
-    );
-  }
+  State<Caretaker> createState() => _CaretakerState();
+}
+
+class _CaretakerState extends State<Caretaker> {TextEditingController nameText = TextEditingController();
+TextEditingController emailText = TextEditingController();
+TextEditingController phoneText = TextEditingController();
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      backgroundColor: Colors.cyanAccent,
+        title: const Text("Add caretaker details",)),
+    body: Column(
+
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+            alignment: Alignment.bottomCenter,
+            margin: const EdgeInsets.only(bottom: 16),
+            child: FloatingActionButton(
+                backgroundColor: Colors.cyanAccent,
+                child: const Icon(Icons.add),
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: const Text("Caretaker details",),
+                        actions: [
+                          TextFormField(
+                            decoration:
+                            const InputDecoration(hintText: "Name"),
+                          ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                                hintText: "Phone Number"),
+                          ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                                hintText: "Email Address"),
+                          ),
+                          TextButton(
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                  MaterialStateProperty.all(
+                                      Colors.cyanAccent)),
+                              onPressed: (){AddCareTakerDetails(name: nameText.text, email: emailText.text, PhnNumber: phoneText.text);
+                              },
+                              child: const Text("Done",
+                              style: TextStyle(color: Colors.black),),)
+                        ],
+                      ));
+                })),
+      ],
+    ),
+  );
+}
+}
+Future AddCareTakerDetails({required String name, required String email,required String PhnNumber})async{
+  try {await FirebaseFirestore.instance.collection("Care Taker").doc(email).set(
+      {'name' : name , 'phone number' : PhnNumber});}
+  on FirebaseAuthException catch(e){String Toast = e.message.toString();
+  Fluttertoast.showToast(msg: Toast);}
 }
