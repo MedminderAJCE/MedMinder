f3da67b8d51a7e8a8783eb5056299d66e7ae3794
ZAMEEL HASSAN <zekyazelu7@gmail.com>
2023-06-18 08:23:51 +0530

    alarm set and home page cards

--- lib/screens/addmedicine.dart
+++ lib/screens/addmedicine.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
-//import 'package:login/screens/setalarm.dart';
-//import 'package:login/screens/setalarm.dart';
-import 'package:login/screens/timepicker.dart';
-import 'package:login/screens/refillremainder.dart';
 import 'package:intl/intl.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:login/screens/refillremainder.dart';
+import 'package:login/screens/timepicker.dart';
 
 class AddMedicine extends StatefulWidget {
-  const AddMedicine({super.key});
+  const AddMedicine({Key? key}) : super(key: key);
 
   @override
-  State<AddMedicine> createState() => _AddMedicineState();
+  _AddMedicineState createState() => _AddMedicineState();
 }
 
 class _AddMedicineState extends State<AddMedicine> {
-  DateTimeRange dateRange =
-      DateTimeRange(start: DateTime(2022, 11, 5), end: DateTime(2022, 12, 24));
+  DateTimeRange dateRange = DateTimeRange(
+    start: DateTime.now(),
+    end: DateTime.now().add(const Duration(days: 1)),
+  );
+  TimeOfDay? selectedTime;
+  bool isAlarmSet = false;
+  final TextEditingController medicationNameController = TextEditingController();
+  final TextEditingController dosageController = TextEditingController();
+  final TextEditingController illnessNameController = TextEditingController();
+  final TextEditingController physicianNameController = TextEditingController();
+
+  @override
+  void dispose() {
+    medicationNameController.dispose();
+    dosageController.dispose();
+    illnessNameController.dispose();
+    physicianNameController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final start = dateRange.start;
@@ -22,188 +40,276 @@ class _AddMedicineState extends State<AddMedicine> {
     final difference = dateRange.duration;
     return Scaffold(
       body: Center(
-          child: SizedBox(
-        width: 300,
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "Specify the name of illness",
-                  disabledBorder: OutlineInputBorder(
+        child: SizedBox(
+          width: 300,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  controller: medicationNameController,
+                  decoration: InputDecoration(
+                    hintText: "Specify the name of medication",
+                    disabledBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(21),
                       borderSide: const BorderSide(
                         color: Color(0xFF00E5FF),
-                      )),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(21),
-                      borderSide: const BorderSide(
-                        color: Color(0xFF00E5FF),
-                      )),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(21),
-                      borderSide: const BorderSide(
-                        color: Color(0xFF00E5FF),
-                      )),
-                ),
-              ),
-              Container(
-                height: 30,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "Provide the name of physician",
-                  disabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(21),
-                      borderSide: const BorderSide(
-                        color: Color(0xFF00E5FF),
-                      )),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(21),
-                      borderSide: const BorderSide(
-                        color: Color(0xFF00E5FF),
-                      )),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(21),
-                      borderSide: const BorderSide(
-                        color: Color(0xFF00E5FF),
-                      )),
-                ),
-              ),
-              Container(
-                height: 30,
-              ),
-              TextField(
-                keyboardType: TextInputType.phone,
-                decoration: InputDecoration(
-                  hintText: "Specify name of the medication",
-                  disabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(21),
-                      borderSide: const BorderSide(
-                        color: Color(0xFF00E5FF),
-                      )),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(21),
-                      borderSide: const BorderSide(
-                        color: Color(0xFF00E5FF),
-                      )),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(21),
-                      borderSide: const BorderSide(
-                        color: Color(0xFF00E5FF),
-                      )),
-                ),
-              ),
-              Container(
-                height: 30,
-              ),
-              TextField(
-                keyboardType: TextInputType.phone,
-                decoration: InputDecoration(
-                  hintText: "Specify Medicine Dosage",
-                  disabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(21),
-                      borderSide: const BorderSide(
-                        color: Color(0xFF00E5FF),
-                      )),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(21),
-                      borderSide: const BorderSide(
-                        color: Color(0xFF00E5FF),
-                      )),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(21),
-                      borderSide: const BorderSide(
-                        color: Color(0xFF00E5FF),
-                      )),
-                ),
-              ),
-              Container(
-                height: 30,
-              ),
-              const SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: pickDateRange,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF00E5FF),
                       ),
-                      child: Text(DateFormat('yyyy/MM/dd').format(start)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(21),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF00E5FF),
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(21),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF00E5FF),
+                      ),
                     ),
                   ),
-                  const SizedBox(width: 12),
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: pickDateRange,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF00E5FF),
+                ),
+                Container(
+                  height: 30,
+                ),
+                TextField(
+                  controller: illnessNameController,
+                  decoration: InputDecoration(
+                    hintText: "Specify the name of illness",
+                    disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(21),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF00E5FF),
                       ),
-                      child: Text(DateFormat('yyyy/MM/dd').format(end)),
                     ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(21),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF00E5FF),
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(21),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF00E5FF),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 30,
+                ),
+                TextField(
+                  controller: physicianNameController,
+                  decoration: InputDecoration(
+                    hintText: "Provide the name of physician",
+                    disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(21),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF00E5FF),
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(21),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF00E5FF),
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(21),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF00E5FF),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 30,
+                ),
+                TextField(
+                  controller: dosageController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    hintText: "Specify Medicine Dosage",
+                    disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(21),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF00E5FF),
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(21),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF00E5FF),
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(21),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF00E5FF),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 30,
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: pickDateRange,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF00E5FF),
+                        ),
+                        child: Text(DateFormat('yyyy/MM/dd').format(start)),
+                      ),
+                    ),
+                    const SizedBox(width: 12),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: pickDateRange,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF00E5FF),
+                        ),
+                        child: Text(DateFormat('yyyy/MM/dd').format(end)),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'Course Duration: ${difference.inDays} days',
+                  style: const TextStyle(fontSize: 20),
+                ),
+                Container(height: 25),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => TimePicker(
+                        onTimeSelected: (time) {
+                          setState(() {
+                            selectedTime = time;
+                            isAlarmSet = true;
+                          });
+                        },
+                      ),
+                    ));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(320.0, 50.0),
+                    backgroundColor: const Color(0xFF00E5FF),
+                  ),
+                  child: const Text(
+                    'Set Alarm',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                if (!isAlarmSet) ...[
+                  const SizedBox(height: 10),
+                  Text(
+                    'Alarm not set',
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                ] else ...[
+                  const SizedBox(height: 16),
+                  Text(
+                    'Alarm set at: ${selectedTime?.format(context) ?? ""}',
+                    style: const TextStyle(fontSize: 20),
                   ),
                 ],
-              ),
-              const SizedBox(height: 16),
-              Text(
-                'Course Duration :${difference.inDays} days',
-                style: const TextStyle(fontSize: 20),
-              ),
-              Container(height: 28),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => const timepicker()));
-                },
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(320.0, 50.0),
-                  backgroundColor: const Color(0xFF00E5FF),
-                ),
-                child: const Text('Set Alarm',
+                Container(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const refillremainder(),
+                    ));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(320.0, 50.0),
+                    backgroundColor: const Color(0xFF00E5FF),
+                  ),
+                  child: const Text(
+                    'Refill Reminder',
                     style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.white)),
-              ),
-              Container(
-                height: 20,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => const refillremainder()));
-                },
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(320.0, 50.0),
-                  backgroundColor: const Color(0xFF00E5FF),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
-                child: const Text('Refill Reminder',
+                SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: saveMedicationDetails,
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(320.0, 50.0),
+                    backgroundColor: Color(0xFF00E5FF),
+                  ),
+                  child: const Text(
+                    'Save Medication Details',
                     style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.white)),
-              ),
-            ],
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
-      )),
+      ),
     );
   }
 
-  Future pickDateRange() async {
-    DateTimeRange? newDateRange = await showDateRangePicker(
-      context: context,
-      initialDateRange: dateRange,
-      firstDate: DateTime(1900),
-      lastDate: DateTime(2100),
+  Future<void> pickDateRange() async {
+    final initialDateRange = DateTimeRange(
+      start: dateRange.start,
+      end: dateRange.end,
     );
+    final newDateRange = await showDateRangePicker(
+      context: context,
+      firstDate: DateTime.now(),
+      lastDate: DateTime(DateTime.now().year + 2),
+      initialDateRange: initialDateRange,
+    );
+    if (newDateRange != null) {
+      setState(() {
+        dateRange = newDateRange;
+      });
+    }
+  }
 
-    if (newDateRange == null) return; // Pressed X
-    setState(() {
-      dateRange = newDateRange;
-    });
+  Future<void> saveMedicationDetails() async {
+    final User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final medicationDetails = {
+        'medicationName': medicationNameController.text,
+        'dosage': dosageController.text,
+        'illnessName': illnessNameController.text,
+        'physicianName': physicianNameController.text,
+        'startDate': dateRange.start,
+        'endDate': dateRange.end,
+        'alarmTime': selectedTime?.format(context), // Convert TimeOfDay to String
+      };
+
+      final CollectionReference medicationCollection = FirebaseFirestore.instance
+          .collection('Medications')
+          .doc(user.email)
+          .collection('Medicines');
+
+      await medicationCollection.add(medicationDetails);
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Medication details saved to Firebase')),
+      );
+    }
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,28 +1,127 @@
 import 'package:flutter/material.dart';
-
-
-//need to change the content according to the data . for a new user home includes :Add your medicine schedules .Later after adding details , they need to be viewed  
-
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  const Home({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
-      body: Center(
-        
-        child: Column(
-           mainAxisAlignment: MainAxisAlignment.center,
-          children:[Text(" Add your medicine schedules",
-        style: TextStyle(
-          fontFamily: 'Railway',
-          fontSize: 30,
-          fontWeight: FontWeight.w700,
-        ),),
-          ]
+    final User? user = FirebaseAuth.instance.currentUser;
+
+    if (user == null) {
+      // User not logged in
+      return Scaffold(
+        body: Center(
+          child: Text(
+            'User not logged in',
+            style: TextStyle(
+              fontFamily: 'Railway',
+              fontSize: 30,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'My Medications',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+            fontSize: 30
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
       ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('Medications')
+            .doc(user.email)
+            .collection('Medicines')
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final List<DocumentSnapshot> documents = snapshot.data!.docs;
+
+          if (documents.isEmpty) {
+            // No medications found
+            return Center(
+              child: Text(
+                'No medications found',
+                style: TextStyle(
+                  fontFamily: 'Railway',
+                  fontSize: 30,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (context, index) {
+              final medication =
+              documents[index].data() as Map<String, dynamic>;
+              final medicationName = medication['medicationName'];
+              final dosage = medication['dosage'];
+              final alarmTime = medication['alarmTime'];
+
+              return Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                elevation: 2,
+                margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        medicationName,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'Dosage: $dosage',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'Alarm Time: $alarmTime',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
