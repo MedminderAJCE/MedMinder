a7f2adb243c2b0e0c481889c3dc00eae71affb30
SreelakshmiAnish <sreelakshmianish20@gmail.com>
2023-06-20 01:06:43 +0530

    Help&Support config

--- lib/screens/HelpandSupport.dart
+++ lib/screens/HelpandSupport.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 class FAQPage extends StatelessWidget {
   final List<QuestionAnswer> faqList = [
+    QuestionAnswer('What is  MedMinder?',
+        'MedMinder is a mobile application designed to help users manage their medication schedules and set reminders for taking prescribed medications. It helps ensure that users take their medications on time and as prescribed by their healthcare providers.'),
+    QuestionAnswer('How does the medicine reminder app work?',
+        'The app allows you to create a personalized medication schedule by adding your medications, dosages, and specific times for reminders. It sends notifications or alarms to remind you when it is time to take your medication. Some apps may also provide additional features like tracking medication adherence and generating reports.'),
     QuestionAnswer(
         'Can different medications have different sounds with the reminders?',
         'At this time you cannot assign different sounds with the reminders.We apologize for any inconvenience caused.'),
@@ -9,7 +13,10 @@ class FAQPage extends StatelessWidget {
         'Yes! To change your email address on your  account, please tap on your avatar on the bottom-right corner of the home screen.You can edit your account and verify the changes.'),
     QuestionAnswer('How can I reset my password?',
         'To reset your password:\n\n *Tap on your profile icon on the bottom right corner of home screen to open your account.\n\n*If you are not logged in tap on "Forgot Password" in the login page.\n\n*Enter your email address and then you will receive an email to reset your password.'),
-    // Add more question-answer pairs as needed
+    QuestionAnswer('Can I add multiple medications to the app?',
+        'Absolutely! Our app supports the management of multiple medications. You can add all your prescribed medications, vitamins, and supplements, along with their respective dosages and schedules.'),
+    QuestionAnswer('How can I contact your team in case of any queries?',''
+        'Attention!, MedMinder support team is unable to answer any medical or medication related questions.\n\n We will be happy to help with any app related questions at medminderajce@gmail.com')
   ];
 
   @override
